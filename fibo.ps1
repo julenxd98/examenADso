0e927926f9e391321e7e963ca8861e65e04a83ff
julenxd98 <55139836+julenxd98@users.noreply.github.com>
2019-12-19 11:48:09 +0100

    Add files via upload

---
+++ fibo.ps1
@@ -0,0 +1,14 @@
+﻿$num1 = [int] (Read-host -Prompt "Escribe un numero")
+$num2 = [int] (Read-host -Prompt "Escribe otro numero")
+
+if ($num1 > $num2) {
+    $num1,$num2=$num2,$num1
+    }
+$pos = Read-Host -Prompt 'Introduce la cantidad números que quieras ver: '
+
+for($i = 1; $i -le $pos; $i++){
+    echo $num2
+    $c = $num1 + $num2
+    $num1 = $num2
+    $num2 = $c
+}
---
+++ parseador.ps1
@@ -0,0 +1,16 @@
+﻿$filepath = '.\powersheell.csv'
+
+$añadir=Import-CSV $filepath 
+foreach ($User in $añadir)
+{
+	$Usuario 	= $User.usuario
+	$Contrasena = $User.contraena
+	$Ape1	    = $User.ape1
+	$Ape2 	    = $User.ape2
+	$OU 		= $User.ou
+}
+	New-ADUser -Name $Usuario -GivenName $ape1 -Surname $ape2 -SamAccountName $Usuario  -Path $ou -AccountPassword(ConvertTo-SecureString $Contrasena -AsPlainText -Force) -Enabled $true       
+
+
+    
+   
