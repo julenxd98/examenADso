835f554d98b0a0e31420aa0263de812de2628113
julenxd98 <55139836+julenxd98@users.noreply.github.com>
2020-02-11 08:56:38 +0100

    Creamos el menu

---
+++ linux/ExamenJulen-Linux.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Creamos el menu con diferentes opciones, introducir los datos, visualizarlos y guardarlos.
+dibujarmenu(){
+echo "-----------------------------------------------------"
+echo "|                  Rellena la frase                 |"
+echo "| Pulsa 1 para añadir nombre de usuario             |"
+echo "| Pulsa 2 para añadir grupo                         |"
+echo "| Pulsa 3 para añadir directorio home               |"
+echo "| Pulsa 4 para añadir el id                         |"
+echo "| Pulsa 5 para ver lo que llevas                    |"
+echo "| Pulsa 7 para añadir el archivo                    |"
+echo "| Pulsa 8 para ver el archivo que añadiremos        |"
+echo "| Pulsa 0 para salir                                |"
+echo "-----------------------------------------------------"
+}
