6628a721cfc70a074c6574fe95be6657c227ef72
julenxd98 <55139836+julenxd98@users.noreply.github.com>
2020-02-11 09:01:06 +0100

    Declaramos variables y primer dialog

--- linux/ExamenJulen-Linux.sh
+++ linux/ExamenJulen-Linux.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#Declaramos las variables necesarias
+let usuario
+let grupo
+let home
+let id
+let todo
 #Creamos el menu con diferentes opciones, introducir los datos, visualizarlos y guardarlos.
 dibujarmenu(){
 echo "-----------------------------------------------------"
@@ -12,4 +18,35 @@ echo "| Pulsa 7 para añadir el archivo                    |"
 echo "| Pulsa 8 para ver el archivo que añadiremos        |"
 echo "| Pulsa 0 para salir                                |"
 echo "-----------------------------------------------------"
+}
+#En este dialog se preguntara por el usuario
+preguntaradaptador(){
+# un archivo temporal donde guardar lo que vamos leyendo
+OUTPUT="/tmp/input.txt"
+
+
+# Dibuja el InputBox
+dialog --title "Añadir usuarios" \
+--backtitle "Nuevo usuario" \
+--inputbox "Indica el nombre de usuario" 8 60 2>$OUTPUT
+
+# lee lo que esta escribiendo el usuario
+response=$?
+
+# lee lo que hay en OUTPUT usando la redirecction  $OUTPUT
+name=$(<$OUTPUT)
+
+#Entra en el case
+case $response in
+#Guarda la variable adaptador y visualiza lo que llevamos
+        0) usuario=$name
+           echo "Por ahora llevas" "usuario="$usuario",grupo="$grupo",home="$home",id="$id
+
+         ;;
+        1) echo "Cancel pressed." ;;
+        255) echo "[ESC] key pressed." ;;
+esac
+
+#borrar archivo temporal
+rm /tmp/input.txt
 }
