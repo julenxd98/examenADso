631853a771235ee22c8fe6fb850935274d579a79
julenxd98 <55139836+julenxd98@users.noreply.github.com>
2020-02-11 08:45:02 +0100

    Add files via upload
    Declaramos e intercambiamos el valor de las variables

---
+++ ExamenJulen-PowerShell.ps1
@@ -0,0 +1,10 @@
+﻿[decimal]$a = Read-Host -Prompt 'Escribe el primer número: '
+[decimal]$b = Read-Host -Prompt 'Escribe el segundo número: '
+
+if([decimal]$a -gt [decimal]$b) {
+           $c = $a
+           $a = $b
+           $b = $c
+        }
+
+echo $a $b
