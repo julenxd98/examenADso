06b8d9a0d3cfc09a5ecd23dd35ff6dd869faf902
julenxd98 <55139836+julenxd98@users.noreply.github.com>
2020-02-11 08:46:52 +0100

    Creamos la suma y la secuencia

--- ExamenJulen-PowerShell.ps1
+++ ExamenJulen-PowerShell.ps1
@@ -1,10 +1,17 @@
-﻿[decimal]$a = Read-Host -Prompt 'Escribe el primer número: '
+[decimal]$a = Read-Host -Prompt 'Escribe el primer número: '
 [decimal]$b = Read-Host -Prompt 'Escribe el segundo número: '
 
-if([decimal]$a -gt [decimal]$b) {
+if([decimal]$a -gt [decimal]$b) { 
            $c = $a
            $a = $b
            $b = $c
         }
 
-echo $a $b
+[decimal]$suma = $a + $b
+        for($x = 1; $x -lt 6; $x++){
+            
+            $c = $a * $b
+            $a = $b
+            $b = $c
+            echo $c
+        }
